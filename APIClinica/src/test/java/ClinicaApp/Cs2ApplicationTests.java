57180a5e76dc6474f57b57d3f3406908e42984c3
EmmanuelCardenasTdeA <emmanuel.cardenas@correo.tdea.edu.co>
2025-04-07 21:57:50 -0500

    INITIAL

---
+++ APIClinica/src/test/java/ClinicaApp/Cs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ClinicaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
